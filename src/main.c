3dbe28de64ff3041fe3b975bc8f0c044e3f231a8
ceoadebi <ceoadebi@outlook.com>
2026-07-24 17:16:40 -0500

    create and open db implementation

--- src/main.c
+++ src/main.c
@@ -8,12 +8,17 @@
 #include "parse.h"
 
 void print_usage(char *argv[]) {
+    printf("Usage: %s -n -f <database file>\n", argv[0]);
+    printf("\t -n  - create new database file\n");
+    printf("\t -f  - (required) path to database file\n");
+    return;
 }
 
 int main(int argc, char *argv[]) { 
     char *filepath = NULL;
     bool newfile = false;    
     int c;
+    int dbfd = -1; // database file descriptor
 
     while ((c = getopt(argc, argv, "nf: ")) != -1) {
         switch (c) {
@@ -30,6 +35,28 @@ int main(int argc, char *argv[]) {
                 return -1;
         }
     }
+    
+    if (filepath == NULL) {
+        printf("Filepath is a required argument\n");
+        print_usage(argv);
+        
+        return 0;
+    }
+
+    if (newfile) {
+        dbfd = create_db_file(filepath);
+        if (dbfd == STATUS_ERROR) {
+            printf("Unable to create database file\n");
+            return -1;
+        }   
+    } else {
+        dbfd = open_db_file(filepath); 
+        if (dbfd == STATUS_ERROR) {
+            printf("Unable to open database file\n");
+            return -1;
+        }
+    }
+
     printf("Newfile: %d\n", newfile);
     printf("Filepath: %s\n", filepath);
 
